4c8140b93c7204b73ee5b8349702a623f30a159b
parhom <andriy.parhomenko@gmail.com>
2018-08-02 22:04:01 +0300

    Fixed migrations numbers

---
+++ common/djangoapps/student/migrations/0011_certificateregenerationrequest.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.utils.timezone
+from django.conf import settings
+from openedx.core.djangoapps.xmodule_django.models import CourseKeyField
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('student', '0010_auto_20170207_0458'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='CertificateRegenerationRequest',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('course_id', CourseKeyField(max_length=255, db_index=True)),
+                ('purpose', models.CharField(max_length=255)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('status', CourseKeyField(max_length=255)),
+                ('modified', models.DateTimeField(default=django.utils.timezone.now)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
