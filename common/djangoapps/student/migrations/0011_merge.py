c0ca1afc10d8d1ee61d9d02053d0fde631138f82
a-parhom <andriy.parhomenko@gmail.com>
2018-08-03 23:52:03 +0300

    Fix migrations

---
+++ common/djangoapps/student/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('student', '0009_auto_20171219_0353'),
+        ('student', '0010_auto_20170207_0458'),
+    ]
+
+    operations = [
+    ]
