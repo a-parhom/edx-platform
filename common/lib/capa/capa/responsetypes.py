66beef7cd09dd851af99eb748ab9ade59a186836
parhom <andriy.parhomenko@gmail.com>
2018-08-06 15:35:44 +0300

    Fixed typo

--- common/lib/capa/capa/responsetypes.py
+++ common/lib/capa/capa/responsetypes.py
@@ -2604,7 +2604,7 @@ class CodeResponse(LoncapaResponse):
     answer = None
     queue_name = None
     
-    def __init__(self, xml, inputfields, context, system, capa_module, minimal_init)
+    def __init__(self, xml, inputfields, context, system, capa_module, minimal_init):
         super(CodeResponse, self).__init__(xml, inputfields, context, system, capa_module, minimal_init)
         self.answer_id = self.answer_ids[-1]
 
