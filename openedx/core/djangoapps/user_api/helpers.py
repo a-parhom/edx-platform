87963411127c468a0b1666b4ab9b83bb812a3007
parhom <andriy.parhomenko@gmail.com>
2020-11-25 14:58:51 +0200

    Phonenumber fixes

--- openedx/core/djangoapps/user_api/helpers.py
+++ openedx/core/djangoapps/user_api/helpers.py
@@ -16,6 +16,8 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.utils.encoding import force_text
 from django.utils.functional import Promise
 
+from phonenumber_field.formfields import PhoneNumberField
+
 LOGGER = logging.getLogger(__name__)
 
 
@@ -95,7 +97,7 @@ class InvalidFieldError(Exception):
 class FormDescription(object):
     """Generate a JSON representation of a form. """
 
-    ALLOWED_TYPES = ["text", "email", "select", "textarea", "checkbox", "plaintext", "password", "hidden"]
+    ALLOWED_TYPES = ["text", "email", "select", "textarea", "checkbox", "plaintext", "password", "hidden", "tel"]
 
     ALLOWED_RESTRICTIONS = {
         "text": ["min_length", "max_length"],
@@ -112,6 +114,7 @@ class FormDescription(object):
         forms.Textarea: "textarea",
         forms.BooleanField: "checkbox",
         forms.EmailField: "email",
+        PhoneNumberField: "tel",
     }
 
     OVERRIDE_FIELD_PROPERTIES = [
